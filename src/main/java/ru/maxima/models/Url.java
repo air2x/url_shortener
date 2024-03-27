9ac5e46855d14ae809619932a4309996f80f3274
Айрат_Шарафиев <sarafievajrat347@gmail.com>
2024-03-27 19:32:28 +0300

    init origin

---
+++ src/main/java/ru/maxima/models/Url.java
@@ -0,0 +1,34 @@
+package ru.maxima.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "url")
+public class Url {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "original_url")
+    @NotEmpty
+    @Size(min = 2, max = 2000, message ="Url min 2 chars, max 2000 chars")
+    private String originalUrl;
+
+    @Column(name = "generated_url")
+    private String generatedUrl;
+
+    @Column(name = "number_of_clicks")
+    private int numberOfClicks;
+}
